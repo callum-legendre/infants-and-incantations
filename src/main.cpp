a5016834e3ce170809c980614ec4bdb85817c1c3
ThrDreen <callumlegendre@gmail.com>
2026-01-28 19:49:37 -0500

    began creating project and first test scene

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Ogre.h>
+#include <OgreApplicationsContext.h>
+#include <OgreInput.h>
+#include <OgreRTShaderSystem.h>
+
+// cretes the application
+class InfantsAndIncantations:
+    // class varaibles of 
+    public OgreBites::ApplicationContext,
+    public OgreBites::InputListener
+{
+private:
+    /* data */
+public:
+    main(/* args */);
+    ~main();
+};
+
+main::main(/* args */)
+{
+}
+
+main::~main()
+{
+}
