365b90d49e5b9f31c62f26c16e93ccdcefe06e9a
Ferro <ferroj2340@my.uwstout.edu>
2026-06-17 17:01:44 -0500

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     std::cout << "Hello, testing. Did Ogre install correctly?" << std::endl;
     std::cout << "catherine was here" << std::endl;
+    std::cout << "Hello this is Joe" << std::endl;
     
     return 0;
 }
