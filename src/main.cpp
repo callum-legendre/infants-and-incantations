9a57d425a9cd5cbc8dffd7ed821afca3e5474f09
Chelsea <czheng987@gmail.com>
2026-06-17 22:25:53 -0400

    Testing/Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main()
     std::cout << "Hello, testing. Did Ogre install correctly?" << std::endl;
     std::cout << "catherine was here" << std::endl;
     std::cout << "Hello this is Joe" << std::endl;
+    std::cout << "Chels here, just testing" << std::endl;
     
     return 0;
 }
