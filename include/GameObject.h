d64f0b31b25430f865b5bdef260416a48e7270e8
ThrDreen <callumlegendre@gmail.com>
2026-08-07 19:12:54 -0400

    created files and began header

---
+++ include/GameObject.h
@@ -0,0 +1,16 @@
+/*
+This is the base class for all obejcts that will exist in the game, meaning every object will inherit from this class.
+*/
+
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include <Ogre.h> // include Ogre
+
+
+class GameObject
+{
+
+}
+
+#endif
