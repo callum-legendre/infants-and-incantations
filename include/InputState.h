b43663dcab9fae568933d5517b867a24fe874ec5
ThrDreen <callumlegendre@gmail.com>
2026-08-08 19:47:29 -0400

    keyboard input
    wrote method in the player class to get the keyboard input. Will be applied in the update function for movement.

---
+++ include/InputState.h
@@ -0,0 +1,13 @@
+#ifndef INPUTSTATE_H
+#define INPUTSTATE_H
+
+struct InputState
+{
+    bool forward = false;
+    bool left = false;
+    bool back = false;
+    bool right = false;
+};
+
+
+#endif
---
+++ src/Player.cpp
@@ -0,0 +1,18 @@
+#include "Player.h"
+
+void Player::update(float deltaTime)
+{
+
+}
+
+InputState Player::getInput()
+{
+    // get the input state
+    const Uint8* keys = SDL_GetKeyboardState(nullptr);
+    InputState* inputState {
+        .forward = keys[SDL_SCANCODE_W],
+        .left = keys[SLD_SCANCODE_A],
+        .back = keys[SDL_SCANCODE_S],
+        .right = keys[SDL_SCANCODE_D]
+    };
+}
