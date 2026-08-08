e971006cb19c38fd6c921cfa7730ba6adb7d045d
ThrDreen <callumlegendre@gmail.com>
2026-08-08 00:10:08 -0400

    created files and outline for player class

---
+++ include/Player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+/*
+The player object that the player controls. 
+It exists outside of all of the LevelScenes, so it survives loading and unloading of them. 
+*/
+
+class Player
+{
+    public:
+
+    private:
+};
+
+#endif
