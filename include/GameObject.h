c15935b45a3d8f53c5c29aa493388911490151f2
ThrDreen <callumlegendre@gmail.com>
2026-08-07 21:10:32 -0400

    optomised includes
    replaced ogre.h with a namespace declaration of SceneNode in order to limit file size

--- include/GameObject.h
+++ include/GameObject.h
@@ -5,7 +5,10 @@ This is the base class for all obejcts that will exist in the game, meaning ever
 #ifndef GAMEOBJECT_H
 #define GAMEOBJECT_H
 
-#include <Ogre.h> // include Ogre
+namespace Ogre // include Ogre
+{
+    class SceneNode;
+}
 
 
 class GameObject
@@ -18,14 +21,15 @@ class GameObject
 
     virtual void update(float deltaTime) // update that runs every frame, is overriden in all necissary objects
     {
-        
+
     }
+    
     virtual ~GameObject() = default; // destructor
 
     protected: // proporties + methods that can be accessed by child classes
     Ogre::SceneNode* sceneNode = nullptr;
 
     private:
-}
+};
 
 #endif
