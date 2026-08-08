b45e001dcd223d4f7fb54db5d1654bc63b12359c
ThrDreen <callumlegendre@gmail.com>
2026-08-07 20:20:29 -0400

    added methods to GameObect.cpp

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,3 +1,16 @@
 #include "GameObject.h"
 
+GameObject::GameObject()
+{
 
+}
+
+void GameObject::update(float deltaTime)
+{
+    // is empty here but in any applicable object will have an override method
+}
+
+GameObject::~GameObject()
+{
+
+}
