b1c5ee85226f0fce9611d1d1e927edab6d849de7
ThrDreen <callumlegendre@gmail.com>
2026-08-08 22:23:03 -0400

    transform.h and update transform
    implimented the Transform.h file where the transfrom class definition lives. Also added an update transform function in GameObject.h that runs on each update.

--- include/GameObject.h
+++ include/GameObject.h
@@ -5,6 +5,8 @@ This is the base class for all obejcts that will exist in the game, meaning ever
 #ifndef GAMEOBJECT_H
 #define GAMEOBJECT_H
 
+#include "Transform.h"
+
 namespace Ogre // include Ogre
 {
     class SceneNode;
@@ -14,22 +16,32 @@ namespace Ogre // include Ogre
 class GameObject
 {
     public:
-    explicit GameObject(Ogre::SceneNode* sceneNode) : sceneNode(sceneNode) // constructor
-    {
+    explicit GameObject(Ogre::SceneNode* sceneNode) : sceneNode(sceneNode) {} // constructor
 
-    }
+    virtual void update(float deltaTime) {} // update that runs every frame, is overriden in all necissary objects
 
-    virtual void update(float deltaTime) // update that runs every frame, is overriden in all necissary objects
-    {
-
-    }
     
     virtual ~GameObject() = default; // destructor
 
     protected: // proporties + methods that can be accessed by child classes
     Ogre::SceneNode* sceneNode = nullptr;
+    Transform transform;
+
 
     private:
+
+    void UpdateTransform()
+    {
+        // only update the transform if something has changed
+        if (transform.IsDirty()){
+            // update transform in scene
+            sceneNode->setPosition(transform.GetPosition());
+            sceneNode->setRotation(transform.GetRotation);
+            sceneNode->setScale(transform.GetScale());
+            // clean the transform
+            transform.ClearDirty();
+        }
+    }
 };
 
 #endif
